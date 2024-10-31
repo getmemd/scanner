88f0dc500e12617149fa180788dba41c5560e907
adil <amedeuyev@gmail.com>
2024-11-01 01:00:32 +0500

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Adilkhan Medeuyev on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
